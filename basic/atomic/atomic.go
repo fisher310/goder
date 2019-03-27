a1adbd30941ea28323f901d60dfbe35ad282e5bc
fisher310 <lihailong699@gmail.com>
2019-03-27 21:54:38 +0800

    The example of sync.Mutex

---
+++ basic/atomic/atomic.go
@@ -0,0 +1,21 @@
+package atomic
+
+import "sync"
+
+type atomicInt struct {
+	value int
+	lock sync.Mutex
+}
+
+func (a *atomicInt) increment() {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	a.value++
+}
+
+func (a *atomicInt) get() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.value
+}
+
---
+++ basic/atomic/atomic_test.go
@@ -0,0 +1,23 @@
+package atomic
+
+import (
+	"testing"
+)
+
+func TestIncrement(t *testing.T) {
+	expected := 2
+	for i := 0; i < 10000; i++ {
+		var a atomicInt
+		a.increment()
+		ch := make(chan int)
+		go func(c chan int) {
+			a.increment()
+			c <- 0
+		}(ch)
+		<- ch
+		if a.get() != expected {
+			t.Errorf("expected value is %d, but the actual is %d\n", expected, a)
+		}
+	}
+}
+
