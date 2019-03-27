7b6c353de88fa115549d404868edf16e18d9357b
fisher310 <lihailong699@gmail.com>
2019-03-27 10:12:58 +0800

    add the basic channel usage

--- channel/channel.go
+++ channel/channel.go
@@ -1,1 +1,56 @@
-package channel
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func createWorker(id int, wg *sync.WaitGroup) worker {
+	w := worker{
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
+	}
+
+	go doWork(id, w)
+	return w
+}
+
+func doWork(id int, w worker) {
+
+	for n := range w.in  {
+		fmt.Printf("worker %d received %c\n", id, n)
+		w.done()
+	}
+}
+
+type worker struct {
+	in chan int
+	done func()
+}
+
+func chanDemo() {
+	var workers [10]worker
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		workers[i] = createWorker(i, &wg)
+	}
+
+	wg.Add(20)
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'a' + i
+	}
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'A' + i
+	}
+
+	wg.Wait()
+}
+
+
+func main() {
+	chanDemo()
+	//bufferedChannel()
+}
