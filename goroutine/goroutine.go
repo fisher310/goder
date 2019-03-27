e80c6ef4e1342de69d9720821420cabf3bea96a1
fisher310 <lihailong699@gmail.com>
2019-03-27 10:16:46 +0800

    some basic useage for goroutine

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,1 +1,19 @@
-package goroutine
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i< 10; i++ {
+		go func(i int) {
+			for {
+				fmt.Printf("hello from goroutine %d\n", i)
+			}
+		}(i)
+	}
+	time.Sleep(time.Minute)
+}
+
+
