ff7a7c8f0689ea16e5f047b5bbc9e8a2ea793d07
fisher310 <lihailong699@gmail.com>
2019-04-02 12:47:06 +0800

    a width first algorithm

--- maze/maze.go
+++ maze/maze.go
@@ -1,1 +1,125 @@
-package maze
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func readMaze(filename string) [][]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	var row, col int
+	fmt.Fscanf(file, "%d %d\n", &row, &col)
+
+	maze := make([][]int, row)
+	for i := range maze {
+		maze[i] = make([]int, col)
+		for j := range maze[i] {
+			fmt.Fscanf(file, "%d", &maze[i][j])
+		}
+		fmt.Fscanf(file, "\n")
+	}
+
+	return maze
+}
+
+type point struct {
+	i, j int
+}
+
+var dirs = [4]point{
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1},
+}
+
+func (p point) add(r point) point {
+	return point{p.i + r.i, p.j + r.j}
+}
+
+func (p point) at(grid [][]int) (int, bool) {
+
+	if p.i < 0 || p.i >= len(grid) {
+		return 0, false
+	}
+
+	if p.j < 0 || p.j >= len(grid[p.i]) {
+		return 0, false
+	}
+
+	return grid[p.i][p.j], true
+}
+
+func walk(maze [][]int, start, end point) [][]int {
+	steps := make([][]int, len(maze))
+	for i := range steps {
+		steps[i] = make([]int, len(maze[i]))
+	}
+
+	Q := []point{start}
+
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+
+		if cur == end {
+			break
+		}
+
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			val, ok := next.at(maze)
+			if !ok || val == 1 {
+				continue
+			}
+
+			val, ok = next.at(steps)
+			if !ok || val != 0 {
+				continue
+			}
+
+			if next == start {
+				continue
+			}
+
+			curStep, _ := cur.at(steps)
+			steps[next.i][next.j] = curStep + 1
+
+			Q = append(Q, next)
+		}
+	}
+
+	return steps
+}
+
+func main() {
+	maze := readMaze("maze/maze.in")
+	for _, row := range maze {
+		for _, val := range row {
+			fmt.Printf("%d ", val)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("--------------------------------------")
+	endPoint := point{len(maze) - 1, len(maze[0]) - 1}
+	steps := walk(maze, point{0, 0}, endPoint)
+	for _, row := range steps {
+		for _, val := range row {
+			fmt.Printf("%3d ", val)
+		}
+		fmt.Println()
+	}
+
+	end := steps[len(steps) - 1][len(steps[0]) - 1]
+
+	fmt.Println(end)
+	//if end > 0 {
+	//	for _, dir := range dirs  {
+	//		prePoint:= endPoint.add(dir)
+	//
+	//	}
+	//	end--
+	//}
+}
