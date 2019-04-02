13e18cd9ed316950c174bd1e2f1a8c67ad2e0744
fisher310 <lihailong699@gmail.com>
2019-04-02 12:46:03 +0800

    a crawler project

--- crawler/main.go
+++ crawler/main.go
@@ -1,1 +1,49 @@
-package crawler
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("error")
+		return
+	}
+
+	reader, e := determineEncoding(resp.Body)
+
+	utf8Reader := transform.NewReader(reader, e.NewEncoder())
+
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", all)
+}
+
+
+func determineEncoding(r io.Reader) (*bufio.Reader, encoding.Encoding) {
+	reader := bufio.NewReader(r)
+	bytes, err := reader.Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _:= charset.DetermineEncoding(bytes, "")
+	return reader, e
+}
